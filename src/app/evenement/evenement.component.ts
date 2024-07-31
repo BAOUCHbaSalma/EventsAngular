85fe07e34fa67bce8c33d6ae63754a9d359d69d5
ilham-jalal <jalalilham55@gmail.com>
2024-07-31 12:08:20 +0100

    j ai ajoutée methode delete event

--- src/app/evenement/evenement.component.ts
+++ src/app/evenement/evenement.component.ts
@@ -1,41 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { EvenementService } from '../service/evenement.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-evenement',
   templateUrl: './evenement.component.html',
-  styleUrl: './evenement.component.css'
+  styleUrls: ['./evenement.component.css']
 })
-export class EvenementComponent implements OnInit{
-  listEvents:any
-  searchForm!:FormGroup
+export class EvenementComponent implements OnInit {
+  listEvents: any;
+  searchForm!: FormGroup;
 
-  constructor(private srv:EvenementService,private fb:FormBuilder){}
+  constructor(private srv: EvenementService, private fb: FormBuilder, private router: Router) {}
+
   ngOnInit(): void {
-    this.initForm()
-    this.srv.showAllEvents().subscribe(value=>{
-      this.listEvents=value
-    })
- 
+    this.initForm();
+    this.loadEvents();
   }
-  initForm(){
-    this.searchForm=this.fb.group({
-      categorie:'',
-      lieu:'',
-      date:''
-    })
 
+  initForm() {
+    this.searchForm = this.fb.group({
+      categorie: '',
+      lieu: '',
+      date: ''
+    });
   }
+
+  loadEvents(): void {
+    this.srv.showAllEvents().subscribe(value => {
+      this.listEvents = value;
+    });
+  }
+
   searchEvents(): void {
     const { categorie, lieu, date } = this.searchForm.value;
     this.srv.SearchEvents(lieu, categorie, date).subscribe(res => {
       this.listEvents = res;
     });
   }
-    
 
+  onUpdate(id: number): void {
+    this.router.navigate(['/event-update', id]);
   }
 
-
-
+  onDelete(id: number): void {
+    this.srv.deleteEvenement(id).subscribe(() => {
+      this.loadEvents();
+    });
+  }
+}
