0a6ed1e819d2c9fbde1e116e8fbf71865e2cccb5
ilham-jalal <jalalilham55@gmail.com>
2024-07-31 16:08:53 +0100

    des modifications

--- src/app/service/evenement.service.ts
+++ src/app/service/evenement.service.ts
@@ -26,22 +26,19 @@ export class EvenementService {
     return this.http.get<Evenement[]>(this.urlApiUserSearch, { params });
   }
 
-  // Retrieve a single event by ID
+
   getEvenementById(id: number): Observable<Evenement> {
     return this.http.get<Evenement>(`${this.urlApi}/evenement/${id}`);
   }
 
-  // Create a new event
   createEvenement(evenement: Evenement): Observable<Evenement> {
     return this.http.post<Evenement>(`${this.urlApi}/admin/evenement`, evenement);
   }
 
-  // Update an existing event
   updateEvenement(id: number, evenement: Evenement): Observable<Evenement> {
     return this.http.put<Evenement>(`${this.urlApi}/admin/evenement/update/${id}`, evenement);
   }
 
-  // Delete an event by ID
   deleteEvenement(id: number): Observable<void> {
     return this.http.delete<void>(`${this.urlApi}/admin/evenement/${id}`);
   }
