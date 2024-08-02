854babc69e2f3d32966371c4007a887b0bad52a9
BAOUCHbaSalma <baouch.salma0@gmail.com>
2024-08-02 22:26:40 +0100

    j'ai modifiee dans la methode reservation

--- src/app/evenement/evenement.component.ts
+++ src/app/evenement/evenement.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { EvenementService } from '../service/evenement.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Erole, Reservation } from '../model/events';
+import { ReservationService } from '../service/reservation.service';
 
 @Component({
   selector: 'app-evenement',
@@ -13,7 +15,7 @@ export class EvenementComponent implements OnInit {
   searchForm!: FormGroup;
   
 
-  constructor(private srv: EvenementService, private fb: FormBuilder, private router: Router) {}
+  constructor(private srv: EvenementService, private fb: FormBuilder, private router: Router,private resrv:ReservationService) {}
 
   ngOnInit(): void {
     this.initForm();
@@ -40,8 +42,37 @@ export class EvenementComponent implements OnInit {
       this.listEvents = res;
     });
   }
-  Reserve(id:Number){
-
+  Reserve(id:number){
+const reservation:Reservation={
+    idReservetion:0,
+    dateReservation:"",
+    heursReservation:"",
+    
+    evenement:{
+      idEvenement: id,
+      titre: '',
+      description: '',
+      lieu: '',
+      dateEvenement: '',
+      prix: 0,
+      categorie: '',
+      heursEvenement: '',
+      image: ''
+    },
+    user:{
+      userId:9,
+      username: '',
+      password: '',
+      age: 0,
+      email: '',
+      role:Erole.USER 
+    }
+   
+  }
+  this.resrv.addReservation(reservation).subscribe(()=>{
+    alert("Reservation")
+    this.ngOnInit()
+  })
   }
 
   
